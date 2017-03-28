1482199ab8608e9302897587109e728b325307fc
Andrij Wilhitecd <andrij.wilhite@gmail.com>
2017-03-28 16:06:39 -0700

    First Commit

---
+++ topspots/topspots/Controllers/topspotsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using Newtonsoft.Json;
+using TopSpots.Models;
+
+namespace TopSpots.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        [HttpGet, Route("api/topspots")]
+        public IHttpActionResult GetAllTopSpots()
+        {
+            string topSpots = File.ReadAllText("C:\\Users\\andrij\\dev\\TopSpotsAPI\\topspots.json");
+
+            TopSpot[] topSpotsArray = JsonConvert.DeserializeObject<TopSpot[]>(topSpots);
+
+            return Ok(topSpotsArray);
+        }
+        [HttpPost]
+        public IHttpActionResult PostTopSpot(TopSpot topSpot)
+        {
+            string topSpots = File.ReadAllText("C:\\Users\\andrij\\dev\\TopSpotsAPI\\topspots.json");
+
+            TopSpot[] topSpotsArray = JsonConvert.DeserializeObject<TopSpot[]>(topSpots);
+
+            List<T> 
+
+            topSpots = JsonConvert.SerializeObject(topSpotsArray);
+
+            File.WriteAllText("C:\\Users\\andrij\\dev\\TopSpotsAPI\\topspots.json", topSpots);
+        }
+    }
+}
